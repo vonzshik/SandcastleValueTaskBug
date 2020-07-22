179a12105144423ff8566d924c6d201ff3070c0c
Nikita Kazmin <nkazmin@syntellect.ru>
2020-07-22 12:12:21 +0300

    Initial commit

---
+++ SandcastleValueTaskBug/BugClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SandcastleValueTaskBug
+{
+    /// <summary>
+    /// Class with bug method
+    /// </summary>
+    public class BugClass
+    {
+        /// <summary>
+        /// Bug method
+        /// </summary>
+        /// <param name="cancellationToken">Token to cancel</param>
+        /// <returns>ValueTask from int</returns>
+        public ValueTask<int> BugMethod(CancellationToken cancellationToken = default) => new ValueTask<int>(0);
+
+        /// <summary>
+        /// Ok method
+        /// </summary>
+        /// <param name="cancellationToken">Token to cancel</param>
+        /// <returns>Task from int</returns>
+        public Task<int> OkMethod(CancellationToken cancellationToken = default) => new Task<int>(() => 0);
+    }
+}
